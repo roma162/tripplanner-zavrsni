f22abcf376fb05b23510e5d1af3e0948106f3dd9
Valentin Romic <romic.valentin@gmail.com>
2023-05-22 21:39:26 +0200

    Edit url

--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -8,9 +8,9 @@ const PostsList = ({ userId, isProfile = false, filterList, userRole }) => {
   const posts = useSelector((state) => state.posts);
   const token = useSelector((state) => state.token);
   const loggedInUserId = useSelector((state) => state.user._id);
-  console.log("App rerendered!")
+
   const getPosts = async () => {
-    const response = await fetch("http://localhost:3001/posts", {
+    const response = await fetch("https://tripplanner-zavrsni.onrender.com/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -20,7 +20,7 @@ const PostsList = ({ userId, isProfile = false, filterList, userRole }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `http://localhost:3001/posts/${userId}/posts`,
+      `https://tripplanner-zavrsni.onrender.com/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
