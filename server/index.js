470f46906e5b2484c90cf3dfca803841e529c724
roma162 <51538565+roma162@users.noreply.github.com>
2023-06-14 02:14:41 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy:"cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({ origin: 'https://tripplanner-zavrsni.onrender.com' }));
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 app.use((req, res, next) => {
