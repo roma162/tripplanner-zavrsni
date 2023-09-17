cb2ab4e9b5accddb609c3137c8c89bae7ebcee4c
roma162 <51538565+roma162@users.noreply.github.com>
2023-09-18 01:17:46 +0200

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getFeedPosts, getUserPosts, likePosts, postComment } from "../controllers/posts.js"
 import { verifyToken } from "../middleware/auth.js";
+import { postDelete } from "../controllers/posts.js";
 
 const router = express.Router();
 
